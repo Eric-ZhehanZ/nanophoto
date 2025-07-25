3386d32216d7661f9a115f244c55279f67d56e4f
张哲涵 Zhehan Zhang <github@zhehanz.com>
2025-07-25 00:53:04 -0400

    Update RepoLink.tsx

--- src/components/RepoLink.tsx
+++ src/components/RepoLink.tsx
@@ -1,18 +1,16 @@
-import { TEMPLATE_REPO_NAME, TEMPLATE_REPO_URL } from '@/app/config';
-import { useAppText } from '@/i18n/state/client';
 import { clsx } from 'clsx/lite';
 import Link from 'next/link';
-import { BiLogoGithub } from 'react-icons/bi';
+import { BiSolidUser } from 'react-icons/bi';
+
+const USER_NAME = 'ZhehanZ';
+const USER_URL = 'https://zhehanz.com';
 
 export default function RepoLink() {
-  const appText = useAppText();
   return (
     <span className="inline-flex items-center gap-2 whitespace-nowrap">
-      <span className="hidden sm:inline-block">
-        {appText.misc.repo}
-      </span>
+      Photos by
       <Link
-        href={TEMPLATE_REPO_URL}
+        href={USER_URL}
         target="_blank"
         className={clsx(
           'flex items-center gap-0.5',
@@ -20,11 +18,11 @@ export default function RepoLink() {
           'hover:text-medium active:text-dim',
         )}
       >
-        <BiLogoGithub
+        <BiSolidUser
           size={16}
           className="translate-y-[0.5px] hidden xs:inline-block"
         />
-        {TEMPLATE_REPO_NAME}
+        {USER_NAME}
       </Link>
     </span>
   );
